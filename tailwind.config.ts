ce0aadac2c3a6514889eca71b0567e2c9fd1c0d5
sujamess <surarerk.boonjit@gmail.com>
2024-05-21 18:50:35 +0700

    🧹 chore: update `tailwind.config.ts`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/core/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: ["cupcake", "dracula"],
+  },
+  plugins: [typography, daisyui],
 };
 export default config;
