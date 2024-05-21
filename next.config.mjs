f6ea0b320e3991d3f32b0f8ee6bf9eb311823df5
sujamess <surarerk.boonjit@gmail.com>
2024-05-21 19:50:19 +0700

    🧹 chore: revert output config in `next.config.mjs`

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import remarkGfm from "remark-gfm";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
