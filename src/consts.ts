b73fe13136b974c796e2dc0f3b64969b82800cb1
Surarerk Boonjit <sujamess@Surarerks-Mac-mini.local>
2025-05-19 20:22:49 +0700

    Update site title and description to personal brand

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "sujamess";
+export const SITE_DESCRIPTION =
+  "Golang enthusiast with an insatiable curiosity for exploring new technologies. While Go is my trusty companion, I'm always on a quest to expand my tech horizons and embrace the thrill of learning different programming paradigms.";
