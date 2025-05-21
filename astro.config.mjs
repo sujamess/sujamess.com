ebda425581218a51a6db2ca6d940bfe540b4926f
Surarerk Boonjit <sujamess@Surarerks-Mac-mini.local>
2025-05-21 22:56:05 +0700

    Add Vercel adapter with web analytics enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sujamess.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://sujamess.com",
+  integrations: [mdx(), sitemap()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
